b3fadd7e3186f8af738cd6b7601814e0e75604eb
Robin Bertuccelli <robin.bertuccelli@gmail.com>
2017-12-20 11:10:15 -0600

    Working Javascript/html no css or jquery

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,40 +1,53 @@
+/*
+var count=30;
 
-alert ("test");
+var counter=setInterval(timer,1000);
 
+function timer() {
+	count=count-1;
+	if (count <= 0) {
+
+	clearInterval(counter);
+}
+document.GetElementbyId("timer").innerHTML=count + "secs";
+}
+*/
+
+    
 function submitAnswers() {
 	var total = 5;
 	var score = 0;
 //Get user input
 
-var q1 = document.forms["quizForm"][q1].value;
-var q2 = document.forms["quizForm"][q2].value;
-var q3 = document.forms["quizForm"][q3].value;
-var q4 = document.forms["quizForm"][q4].value;
-var q5 = document.forms["quizForm"][q5].value;
+var q1 = document.forms["quizForm"]["q1"].value;
+var q2 = document.forms["quizForm"]["q2"].value;
+var q3 = document.forms["quizForm"]["q3"].value;
+var q4 = document.forms["quizForm"]["q4"].value;
+var q5 = document.forms["quizForm"]["q5"].value;
 
-alert("q1");
-
+ 
 //Validation
 for (i=1; i <= total; i++){
-if(eval("q" +i) == null || eval("q" +i) =="") {
-	alert("you missed question "+ i);
+if(eval('q' +i) == null || eval('q' +i) == '') {
+	alert('you missed question '+ i);
 	return false;
 	}
-}
-//Set Correct answers
+} 
+ //Set Correct answers
 var answers = ["b", "b", "c", "a", "d"];
 
 //Check Answers
 for (i=1; i <= total; i++){
-	if(eval("q" + i) == answers[i-1]){
+	if(eval('q' + i) == answers[i-1]){
 		score++;
 	}
 }
 
 //Display Results
-var results = document.getElementById("results");
-results.innerHTML = '<h3>You scored <span>' +score+ '</span> Out of <span>'+total+'</span></h3>';
+var results = document.getElementById('results');
+results.innerHTML = '<h3>You scored <span>' +score+ '</span> out of <span>'+total+'</span></h3>';
 
-alert("You scored" +score+ "out of" +total);
+alert('You scored' + score + 'out of' + total);
 
 return false;
+}
